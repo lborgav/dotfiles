8583581ffeff1ea8ae988711d0d12ae54338607a
Leonardo Borges Avelino <lborgav@gmail.com>
2018-05-02 15:22:26 -0300

    Adding brew.sh

---
+++ brew.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Updating and Upgrading
+brew update
+brew upgrade
+
+# Installing productivity steroids
+
+brew install git
+brew install the_silver_searcher
+brew install tree
+brew install zsh
+
+# Cleanup
+brew cleanup
