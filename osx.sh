52faf753d1ef34ba20d80c14c5638e36f5407505
Leonardo Borges Avelino <lborgav@gmail.com>
2018-05-03 14:50:19 -0300

    Adding OS X

---
+++ osx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+###################################################################
+# Finder
+###################################################################
+
+# Show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
