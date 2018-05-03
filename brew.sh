f3be4f566c7448b68504b317e403b4f3f1851d67
Leonardo Borges Avelino <lborgav@gmail.com>
2018-05-03 01:03:01 -0300

    Adding taps to brew

--- brew.sh
+++ brew.sh
@@ -11,5 +11,10 @@ brew install the_silver_searcher
 brew install tree
 brew install zsh
 
+# Taps
+brew tap caskroom/cask
+brew tap caskroom/fonts
+brew tap caskroom/versions
+
 # Cleanup
 brew cleanup
