f5db9402a0c1ef89813dcd2b50e6d474a0e54222
Leonardo Borges Avelino <lborgav@gmail.com>
2018-05-14 00:12:47 -0300

    Adding config alias

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
 alias vim="nvim"
 alias weather='curl -4 http://wttr.in/Sao_Paulo'
 alias moon='curl -4 http://wttr.in/Moon'
+alias config='/usr/bin/git --git-dir=/Users/leonardoborges/.cfg/ --work-tree=/Users/leonardoborges'
