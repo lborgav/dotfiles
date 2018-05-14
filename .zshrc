53429526e601a90feba4b4b9eb47131c72aad00b
Leonardo Borges Avelino <lborgav@gmail.com>
2018-05-14 00:13:37 -0300

    Adding exports and .zshrc

---
+++ .zshrc
@@ -0,0 +1,24 @@
+# Exports
+. ./exports.sh
+
+ZSH_THEME="robbyrussell"
+CASE_SENSITIVE="false"
+ENABLE_CORRECTION="false"
+COMPLETION_WAITING_DOTS="true"
+
+plugins=(git docker tmux)
+
+. $ZSH/oh-my-zsh.sh
+
+# z jump
+. /usr/local/etc/profile.d/z.sh
+
+# Aliases
+. ./aliases.sh
+
+eval "$(jenv init -)"
+
+### Added by IBM Cloud CLI
+source /usr/local/Bluemix/bx/zsh_autocomplete
+
+. "/usr/local/opt/nvm/nvm.sh"
---
+++ exports.sh
@@ -0,0 +1,29 @@
+# ssh
+export SSH_KEY_PATH="~/.ssh/id_rsa"
+
+export MANPATH="/usr/local/man:$MANPATH"
+
+export EDITOR=nvim
+
+export LANG=en_US.UTF-8
+
+# Android Studio
+export ANDROID_HOME=~/Library/Android/sdk
+export PATH=$PATH:~/Library/Android/sdk/platform-tools
+export PATH=$PATH:~/Library/Android/sdk/tools
+
+# Fastlane
+export PATH="$HOME/.fastlane/bin:$PATH"
+
+# Path to your oh-my-zsh installation.
+export ZSH=~/.oh-my-zsh
+
+export PATH="$HOME/.jenv/bin:$PATH"
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+
+# RabbitMQ
+PATH=$PATH:/usr/local/sbin
+
+# Node version manager
+export NVM_DIR="$HOME/.nvm"
