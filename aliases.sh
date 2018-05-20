7f4acbd5f15812dd913fcfb69b705dc2223745da
Leonardo Borges Avelino <lborgav@gmail.com>
2018-05-20 02:38:54 -0300

    Adding alias and fixing .zshrc

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,4 @@ alias vim="nvim"
 alias weather='curl -4 http://wttr.in/Sao_Paulo'
 alias moon='curl -4 http://wttr.in/Moon'
 alias config='/usr/bin/git --git-dir=/Users/leonardoborges/.cfg/ --work-tree=/Users/leonardoborges'
+alias server='python -m "SimpleHTTPServer" 8000'
