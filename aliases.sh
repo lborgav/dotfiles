e2c2282fa8f4a1f821e6c9d0808ce46e1a66f65b
Leonardo Borges Avelino <lborgav@gmail.com>
2018-06-03 15:33:20 -0300

    Fixing alias

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 alias vim="nvim"
 alias weather='curl -4 http://wttr.in/Sao_Paulo'
 alias moon='curl -4 http://wttr.in/Moon'
-alias config='/usr/bin/git --git-dir=/Users/leonardoborges/.cfg/ --work-tree=/Users/leonardoborges'
+alias dotfiles='/usr/bin/git --git-dir=${HOME}/.df/ --work-tree=${HOME}'
 alias server='python -m "SimpleHTTPServer" 8000'
