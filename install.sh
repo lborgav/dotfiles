ac318b6c2ec5c29c5af83dbe1ead6c1d8ae02f6e
Leonardo Borges Avelino <lborgav@gmail.com>
2025-11-12 13:15:04 -0300

    ...

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ echo -e "$PREFIX_INFO Checking for conflicts..."
 conflicts=$(dotfiles checkout 2>&1 | egrep "\s+\." | awk '{print $1}')
 
 if [ -n "$conflicts" ]; then
-  echo -e "$PREFIX_WARN Conflicts found. Moving conflicted files to backup..."
+  echo -e "$PREFIX_WARN Conflicts found. Moving conflicted files to backup ..."
   echo "$conflicts" | while read -r file; do
     mkdir -p "$HOME/.dotfiles-backup/$(dirname "$file")"
     mv "$HOME/$file" "$HOME/.dotfiles-backup/$file"
